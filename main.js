f1dddbb948511f924415845a3d1acfd4727d8c5a
khalidjambi <khalid.jambi18@gmail.com>
2019-05-06 14:38:33 +0700

    halo

---
+++ main.js
@@ -0,0 +1,14 @@
+// $('h1').hide();
+// $('h1#heading1').hide();
+// $('h1.heading2').hide();
+
+// $('span').css('color', 'red');
+$('ul#list li:first').css('color', 'red');
+$('ul#list li:last').css('color', 'green');
+$('ul#list li:even').css('background-color', 'yellow');
+$('ul#list li:odd').css('background-color', '#cccccc');
+$('ul#list li:nth-child(3n)').css('list-style', 'none');
+$(':text').hide();
+$('[href]').css('color', 'red');
+$('a[href="http://yahoo.com"]').css('color', 'green'); 
+// $('*').hide();
